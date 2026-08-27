934769054240cd4abeb0d75f2d41d8a0a1e9937c
gideon-kingsly <gideonr.raj@gmail.com>
2026-08-27 13:11:31 +0530

    Prepare frontend for production API

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,6 @@
-const API = "http://127.0.0.1:8000";
+const API = window.location.hostname === "localhost"
+    ? "http://127.0.0.1:8000"
+    : "YOUR_RENDER_BACKEND_URL";
 
 console.log("CloudDiag JavaScript loaded successfully");
 
