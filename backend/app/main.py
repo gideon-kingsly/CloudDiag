55530095f3e14b09fe28c893469e0287cdf827ce
gideon-kingsly <gideonr.raj@gmail.com>
2026-08-27 14:28:53 +0530

    Allow production frontend CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,9 +25,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5500",
-        "http://127.0.0.1:5500"
-    ],
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://clouddiag.onrender.com"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
